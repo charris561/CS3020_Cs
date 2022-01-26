0cac9a484b6e578dedf9b0eabbbf46309cbea485
charris561 <89329141+charris561@users.noreply.github.com>
2022-01-26 12:39:03 -0700

    Programming challenge 1

---
+++ In Class Challenges/Charris4Challenge1/Gameboard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charris4Challenge1
+{
+    public class Gameboard
+    {
+
+        char[,] grid = new char[10, 10];
+        
+        
+        public void FillGrid(){
+
+        for (int row = 0; row < grid.GetLength(0); row++){
+
+            for (int col = 0; col < grid.GetLength(1); col++){
+
+                    grid[row, col] = 'X';
+
+                }//For Columns
+
+            }//For Rows
+
+        }//FillGrid
+
+        public void Display() 
+        {
+
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                DrawLine();
+
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+
+                    Console.Write($"| {grid[row, col]} ");
+
+                }//for col
+
+                Console.WriteLine("|");
+
+            }//for row
+
+            DrawLine();
+
+        }//Display
+
+        private void DrawLine()
+        {
+
+            for (int i = 0; i < grid.GetLength(1) * 4 + 1; i++)
+            {
+                Console.Write($"-");
+            }
+            Console.WriteLine();
+
+        }//DrawLine
+
+    }//Gameboard
+
+}//Charris4Challenge1
---
+++ In Class Challenges/Charris4Challenge1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Charris4Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gameboard board = new Gameboard();
+            board.FillGrid();
+            board.Display();
+
+        }//Main
+
+    }//Program
+
+}//Charris4Challenge1
+
+
+
+
+//Dude this is
