d886d76a3ca474fb3a32a3daaf493143385549e7
charris561 <charris4@uccs.edu>
2022-02-09 12:08:59 -0700

    simstudent

---
+++ In Class Challenges/SimStudent2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimStudent2
+{
+
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Student student = new Student();
+            student.Name = "Test Name";
+            Console.WriteLine($"student.Name = {student.Name}");
+        }
+    }
+
+}
---
+++ In Class Challenges/SimStudent2/Student.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimStudent2
+{
+    public class Student
+    {
+        private string name;
+        private float gpa;
+        private double money;
+        private int energy;
+        private int happiness;
+        private int fullness;
+
+        public string Name { get => name; set => name = value; }
+
+        public float Gpa
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public double Money
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Energy
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Happiness
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Fullness
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Chill()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Eat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Party()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Sleep()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Study()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
